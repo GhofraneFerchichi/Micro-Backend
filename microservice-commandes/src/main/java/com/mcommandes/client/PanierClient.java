b917811a3c997a06b650328230427269b7c5fa67
GhofraneFerchchi <ghofranefer01@gmail.com>
2024-05-21 23:04:06 +0100

    add argocd image updater config

--- microservice-commandes/src/main/java/com/mcommandes/client/PanierClient.java
+++ microservice-commandes/src/main/java/com/mcommandes/client/PanierClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "mpanier", url = "${application.config.mpanier-url}")
+@FeignClient(name = "mpanier", url = "${spring.config.mpanier-url}")
 @Component
 public interface PanierClient {
     @GetMapping("/api/v1/mpanier/paniers/{id}")
