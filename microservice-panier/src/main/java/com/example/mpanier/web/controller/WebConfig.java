39b5c54ec9414afdf5caab06fe0086793fb6bcc4
GhofraneFerchchi <ghofranefer01@gmail.com>
2024-06-05 15:10:08 +0100

    fix images

--- microservice-panier/src/main/java/com/example/mpanier/web/controller/WebConfig.java
+++ microservice-panier/src/main/java/com/example/mpanier/web/controller/WebConfig.java
@@ -1,18 +1,17 @@
 package com.example.mpanier.web.controller;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/v1/mpanier/**")
+        registry.addMapping("/**")  // Allow all paths
                 .allowedOrigins("*")  // Allow all origins
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")  // Allow all headers
-                .allowCredentials(false)
-                .maxAge(3600);
+                .allowedMethods("*")  // Allow all methods
+                .allowedHeaders("*"); // Allow all headers
     }
 }
