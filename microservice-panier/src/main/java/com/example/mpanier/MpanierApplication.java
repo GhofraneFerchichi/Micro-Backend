9bf8219a70f36efe832399c7c4da88b695da34ce
GhofraneFerchchi <ghofranefer01@gmail.com>
2024-06-06 09:35:39 +0100

    fix images

--- microservice-panier/src/main/java/com/example/mpanier/MpanierApplication.java
+++ microservice-panier/src/main/java/com/example/mpanier/MpanierApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
-@EnableJpaRepositories
 
 public class MpanierApplication {
 
